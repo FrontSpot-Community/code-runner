34c468dd5e95802459eefcaae830a9132a3de271
Andrey Lugovtsov <andrey.lugovtsov@gmail.com>
2018-03-25 11:40:51 +0300

    container array. code-runner-php container added

--- bin/runContainers.sh
+++ bin/runContainers.sh
@@ -3,20 +3,30 @@
 # Exit script with error if it is occured
 set -e
 
-docker_image_name="code-runner-node"
+# Associative array with Docker container_name: image_name values
+declare -A docker_image_names=(
+    # Container name: Docker image name from Code Wars registry
+    # see https://github.com/Codewars/codewars-runner-cli for existing images names.
+    ["code-runner-node"]="node-runner"
+    ["code-runner-php"]="alt-runner"
+)
 
-# Check if container exist
-if [ ! "$(docker ps -q -f name=${docker_image_name})" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=${docker_image_name})" ]; then
-        echo "Removing container because current one is stopped:"
-        docker rm "${docker_image_name}"
+for docker_container_name in "${!docker_image_names[@]}"; do
+    docker_image_name=${docker_image_names[$docker_container_name]}
+
+    # Check if container exist
+    if [ ! "$(docker ps -q -f name=${docker_container_name})" ]; then
+        if [ "$(docker ps -aq -f status=exited -f name=${docker_container_name})" ]; then
+            echo "Removing ${docker_container_name} container because current one is stopped:"
+            docker rm "${docker_container_name}"
+        else
+          echo "Container doesn't exist."
+        fi
+
+        echo "Creating new ${docker_container_name} container:"
+        docker run -it -d --name="$docker_container_name" --entrypoint /bin/bash codewars/${docker_image_name}
+        echo "Container created."
     else
-      echo "Container doesn't exist."
+      echo "Nothing to do. Container already runned."
     fi
-
-    echo "Creating new container:"
-    docker run -it -d --name=code-runner-node --entrypoint /bin/bash codewars/node-runner
-    echo "Container created."
-else
-  echo "Nothing to do. Container already runned."
-fi
+done
