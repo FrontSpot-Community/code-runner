987796220038e3c3938ba75d4ecc60cd3306df14
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2019-03-06 02:33:10 +0300

    Update example to run node with babel and jsx compilation

--- experiments/execudeCodeOnContainer.js
+++ experiments/execudeCodeOnContainer.js
@@ -2,10 +2,12 @@ const { executeTestOnContainer } = require('../app/runner/dockerRunner');
 
 const task = {
   language: 'javascript',
-  code: 'var a = 1;',
-  fixture: 'Test.assertEquals(a, 1)',
+  languageVersion: '8.x/babel',
+  code: "const React = require('react'); const a = <h1>Hello, world</h1>;",
+  fixture: "Test.assertEquals(a.props.children, 'Hello, world')",
   testFramework: 'cw'
 };
 
-executeTestOnContainer('code-runner-node', task)
-  .then(result => console.log(result));
+executeTestOnContainer('code-runner-node', task).then(result =>
+  console.log(result)
+);
